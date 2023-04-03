6c0c22d030c190a9e9563611164ede9825417783
arcausin <arcausin@gmail.com>
2023-04-03 19:26:54 +0200

    make include template for header and footer for development

---
+++ include/footer.php
@@ -0,0 +1,24 @@
+<footer id="footer-content" class="container-fluid bg-c22015">
+    <div class="row">
+        <div class="col-12 col-lg-4 align-self-center">
+            <a class="text-decoration-none animate-opacity" href="/"><p class="mt-2 mb-2 hardome text-white fs-4 text-center text-lg-start ms-lg-5">Festival des Films en Plein Air</p></a>
+        </div>
+
+        <div class="col-12 col-lg-4 align-self-lg-center">
+            <nav class="navbar-expand-lg mx-2 text-center mx-md-5 mx-lg-0 mt-2 mb-2">
+                <ul class="navbar-nav d-lg-flex justify-content-lg-around">
+                    <li class="nav-item"><a class="text-decoration-none nav-link text-white animate-opacity" href="/mentions-legales">Mentions légales</a></li>
+                    <li class="nav-item"><a class="text-decoration-none nav-link text-white animate-opacity" href="/politique-de-confidentialites">Politique de confidentialités</a></li>
+                    <li class="nav-item"><a class="text-decoration-none nav-link text-white animate-opacity" href="/contact">Contact</a></li>
+                </ul>
+            </nav>
+        </div>
+        
+        <div class="col-12 col-lg-4 d-lg-flex justify-content-lg-around mt-2 mb-2">
+            <p class="text-center text-white align-self-center mb-0">Du 5 au 8 août 2023</p>
+            <p class="text-center text-white align-self-center mb-0">Parc Monceau <br>
+            35 Boulevard de Courcelles <br>
+            75008 Paris</p>
+        </div>
+    </div>
+</footer>
---
+++ include/header.php
@@ -0,0 +1,26 @@
+<header id="header-content" class="container-fluid bg-white fixed-top">
+    <nav class="navbar navbar-expand-lg">
+        <a class="navbar-brand animate-opacity" href="/"><h1 class="hardome text-c22015 fs-4 ms-lg-5">Festival des Films en Plein Air</h1></a>
+        
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarMenu" aria-controls="navbarMenu" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarMenu">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <a href="/le-festival.php" class="nav-link imperial text-black animate-opacity ps-0">Le festival</a>
+                </li>
+                <li class="nav-item">
+                    <a href="/actualites.php" class="nav-link imperial text-black animate-opacity">Actualités</a>
+                </li>
+                <li class="nav-item">
+                    <a href="/films.php" class="nav-link imperial text-black animate-opacity">Films</a>
+                </li>
+                <li class="nav-item">
+                    <a href="/reservation.php" class="nav-link imperial text-black animate-opacity pe-0 me-lg-5">Réservation</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
