25c91a1aa5ba6255c03b75e6b95a67a40b025bc0
arcausin <arcausin@gmail.com>
2023-04-04 18:03:14 +0200

    update news page

--- actualites/index.php
+++ actualites/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Actualités | Films en Plein Air</title>
+    <title>Actualités - Films en Plein Air</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="/styles.css">
 
@@ -24,17 +24,17 @@
     </script>
 </head>
 <body>
-    <?php require_once("include/header.php"); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/include/header.php'); ?>
 
     <div class="bg-f7f4ee">
         <div id="main-content" class="container">
             <main class="row">
-                <section class="col-12 mb-3">
+                <section class="col-12">
                     <p class="mt-3 mb-3 text-black"><a class="text-black animate-opacity" href="/">Accueil</a> > Actualités</p>
 
-                    <h2 class="mt-5 mb-5 dinengschriftstd text-black text-center fs-1">Actualités</h2>
+                    <h1 class="mt-5 mb-5 imperial-bold text-black text-center">Actualités</h1>
 
-                    <p class="imperial text-black mb-3">Retrouvez toutes les actualités concernant le Festival des Films en Plein Air</p>
+                    <p class="imperial text-black mb-5">Retrouvez toutes les actualités concernant le Festival des Films en Plein Air ce tenant du 05 au 08 août 2023.</p>
 
                     <div class="row g-4 mb-4">
                         <div class="col-12 col-md-6 col-lg-4">
@@ -48,6 +48,7 @@
                                 </div>
                             </a>
                         </div>
+
                         <div class="col-12 col-md-6 col-lg-4">
                             <a class="text-decoration-none text-white animate-opacity" href="/">
                                 <div class="shadow position-relative">
@@ -59,6 +60,7 @@
                                 </div>
                             </a>
                         </div>
+
                         <div class="col-12 col-md-6 col-lg-4">
                             <a class="text-decoration-none text-white animate-opacity" href="/">
                                 <div class="shadow position-relative">
@@ -71,12 +73,26 @@
                             </a>
                         </div>
                     </div>
+
+                    <div class="mb-5 text-center">
+                        <a href="/reservation">
+                            <span class="badge text-bg-dark py-2 fs-3 animate-opacity">
+                                <h3 class="imperial-bold mb-0 fw-bold">Réserver</h3>
+                            </span>
+                        </a>
+                    </div>
+
+                    <h2 class="mb-3 imperial-bold text-black text-center text-lg-start">Plan du festival</h2>
+
+                    <div class="mb-5">
+                        <iframe class="w-100" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2623.873553136649!2d2.306766315824663!3d48.879687007160825!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66fbe98f714c3%3A0xe62425fddeddc402!2sParc%20Monceau!5e0!3m2!1sfr!2sfr!4v1680574682039!5m2!1sfr!2sfr" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
                 </section>
             </main>
         </div>
     </div>
 
-    <?php require_once("include/footer.php"); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/include/footer.php'); ?>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
