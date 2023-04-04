f03cb0e7690f04c244af29ae6a142f8ff6b90731
arcausin <arcausin@gmail.com>
2023-04-04 18:02:43 +0200

    update homepage

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Accueil | Films en Plein Air</title>
+    <title>Accueil - Films en Plein Air</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="/styles.css">
 
@@ -29,8 +29,12 @@
     <div class="bg-f7f4ee">
         <div id="main-content" class="container">
             <main class="row">
-                <section class="col-12 mb-3">
-                    <h2 class="mt-5 mb-5 dinengschriftstd text-black fs-1">Nos derniers articles</h2>
+                <div class="col-12 text-center mt-3 mb-3">
+                    <h1 class="fs-6 imperial-bold text-black">Bienvenue sur films-en-plein-air.org</h1>
+                </div>
+                
+                <section class="col-12">
+                    <h2 class="mb-3 imperial-bold text-black fs-2">Nos derniers articles</h2>
 
                     <div class="row g-4 mb-4">
                         <div class="col-12 col-md-6">
@@ -39,7 +43,7 @@
                                     <img class="img-fluid rounded" src="https://st.depositphotos.com/1003368/5028/i/950/depositphotos_50283209-stock-photo-street-in-paris-illustration.jpg" alt="" style="filter: brightness(0.85);">
 
                                     <div class="p-3 position-absolute bottom-0">
-                                        <h3 class="fs-4 mb-0">Test d'un article du festival</h3>
+                                        <h3 class="fs-4 dinengschriftstd mb-0">Test d'un article du festival</h3>
                                     </div>
                                 </div>
                             </a>
@@ -50,92 +54,113 @@
                                     <img class="img-fluid rounded" src="https://st.depositphotos.com/1003368/5028/i/950/depositphotos_50283209-stock-photo-street-in-paris-illustration.jpg" alt="" style="filter: brightness(0.85);">
 
                                     <div class="p-3 position-absolute bottom-0">
-                                        <h3 class="fs-4 mb-0">Test d'un article du festival</h3>
+                                        <h3 class="fs-4 dinengschriftstd mb-0">Test d'un article du festival</h3>
                                     </div>
                                 </div>
                             </a>
                         </div>
                     </div>
 
-                    <div class="mb-4 text-center">
-                        <button type="button" class="btn btn-outline-dark btn-lg bg-white border-0 animate-opacity text-black imperial-bold shadow">Plus d'articles</button>
+                    <div class="mb-5 text-center">
+                        <a href="/actualites">
+                            <span class="badge text-bg-dark py-2 fs-3 animate-opacity">
+                                <h3 class="imperial-bold mb-0 fw-bold">Plus d'articles</h3>
+                            </span>
+                        </a>
                     </div>
                 </section>
 
-                <section class="col-12 mb-3">
-                    <h2 class="mt-5 mb-5 dinengschriftstd text-black fs-1">Les prochaines projections</h2>
+                <section class="col-12">
+                    <h2 class="mb-3 imperial-bold text-black fs-2">Les prochaines projections</h2>
 
                     <div class="row g-4 mb-4">
-                        <div class="col-6 col-md-3 col-lg-2">
-                            <a class="text-decoration-none text-white animate-opacity" href="/">
-                                <div class="shadow position-relative">
-                                    <img class="img-fluid rounded" src="https://fr.web.img6.acsta.net/c_310_420/pictures/17/09/26/15/21/0089085.jpg" alt="" style="filter: brightness(0.85);">
-                                    
-                                    <div class="p-3 w-100 position-absolute bottom-0">
-                                        <h3 class="fs-6 text-center mb-0">A Ghost Story</h3>
+                        <div class="col-6 col-md-4 col-lg-2">
+                            <a class="text-decoration-none text-white animate-opacity" href="/films/paranoia">
+                                <div class="rounded shadow">
+                                    <img class="img-fluid rounded-top" src="/images/films/paranoia.jpg" alt="">
+                                    <div class="p-3">
+                                        <h3 class="fs-6 text-black text-center mb-0">05/08/2023 - 18h00</h3>
                                     </div>
                                 </div>
                             </a>
                         </div>
-                        <div class="col-6 col-md-3 col-lg-2">
-                            <a class="text-decoration-none text-white animate-opacity" href="/">
-                                <div class="shadow position-relative">
-                                    <img class="img-fluid rounded" src="https://fr.web.img6.acsta.net/c_310_420/pictures/17/09/26/15/21/0089085.jpg" alt="" style="filter: brightness(0.85);">
-                                    
-                                    <div class="p-3 w-100 position-absolute bottom-0">
-                                        <h3 class="fs-6 text-center mb-0">A Ghost Story</h3>
+
+                        <div class="col-6 col-md-4 col-lg-2">
+                            <a class="text-decoration-none text-white animate-opacity" href="/films/nocturnal-animals">
+                                <div class="rounded shadow">
+                                    <img class="img-fluid rounded-top" src="/images/films/nocturnal_animals.jpg" alt="">
+                                    <div class="p-3">
+                                        <h3 class="fs-6 text-black text-center mb-0">05/08/2023 - 20h00</h3>
                                     </div>
                                 </div>
                             </a>
                         </div>
-                        <div class="col-6 col-md-3 col-lg-2">
-                            <a class="text-decoration-none text-white animate-opacity" href="/">
-                                <div class="shadow position-relative">
-                                    <img class="img-fluid rounded" src="https://fr.web.img6.acsta.net/c_310_420/pictures/17/09/26/15/21/0089085.jpg" alt="" style="filter: brightness(0.85);">
-                                    
-                                    <div class="p-3 w-100 position-absolute bottom-0">
-                                        <h3 class="fs-6 text-center mb-0">A Ghost Story</h3>
+
+                        <div class="col-6 col-md-4 col-lg-2">
+                            <a class="text-decoration-none text-white animate-opacity" href="/films/prisoners">
+                                <div class="rounded shadow">
+                                    <img class="img-fluid rounded-top" src="/images/films/prisoners.jpg" alt="">
+                                    <div class="p-3">
+                                        <h3 class="fs-6 text-black text-center mb-0">05/08/2023 - 22h00</h3>
                                     </div>
                                 </div>
                             </a>
                         </div>
-                        <div class="col-6 col-md-3 col-lg-2">
-                            <a class="text-decoration-none text-white animate-opacity" href="/">
-                                <div class="shadow position-relative">
-                                    <img class="img-fluid rounded" src="https://fr.web.img6.acsta.net/c_310_420/pictures/17/09/26/15/21/0089085.jpg" alt="" style="filter: brightness(0.85);">
-                                    
-                                    <div class="p-3 w-100 position-absolute bottom-0">
-                                        <h3 class="fs-6 text-center mb-0">A Ghost Story</h3>
+
+                        <div class="col-6 col-md-4 col-lg-2">
+                            <a class="text-decoration-none text-white animate-opacity" href="/films/dual">
+                                <div class="rounded shadow">
+                                    <img class="img-fluid rounded-top" src="/images/films/dual.jpg" alt="">
+                                    <div class="p-3">
+                                        <h3 class="fs-6 text-black text-center mb-0">06/08/2023 - 18h00</h3>
                                     </div>
                                 </div>
                             </a>
                         </div>
-                        <div class="col-6 col-md-3 col-lg-2">
-                            <a class="text-decoration-none text-white animate-opacity" href="/">
-                                <div class="shadow position-relative">
-                                    <img class="img-fluid rounded" src="https://fr.web.img6.acsta.net/c_310_420/pictures/17/09/26/15/21/0089085.jpg" alt="" style="filter: brightness(0.85);">
-                                    
-                                    <div class="p-3 w-100 position-absolute bottom-0">
-                                        <h3 class="fs-6 text-center mb-0">A Ghost Story</h3>
+
+                        <div class="col-6 col-md-4 col-lg-2">
+                            <a class="text-decoration-none text-white animate-opacity" href="/films/a-ghost-story">
+                                <div class="rounded shadow">
+                                    <img class="img-fluid rounded-top" src="/images/films/a_ghost_story.jpg" alt="">
+                                    <div class="p-3">
+                                        <h3 class="fs-6 text-black text-center mb-0">06/08/2023 - 19h45</h3>
                                     </div>
                                 </div>
                             </a>
                         </div>
-                        <div class="col-6 col-md-3 col-lg-2">
-                            <a class="text-decoration-none text-white animate-opacity" href="/">
-                                <div class="shadow position-relative">
-                                    <img class="img-fluid rounded" src="https://fr.web.img6.acsta.net/c_310_420/pictures/17/09/26/15/21/0089085.jpg" alt="" style="filter: brightness(0.85);">
-                                    
-                                    <div class="p-3 w-100 position-absolute bottom-0">
-                                        <h3 class="fs-6 text-center mb-0">A Ghost Story</h3>
+
+                        <div class="col-6 col-md-4 col-lg-2">
+                            <a class="text-decoration-none text-white animate-opacity" href="/films/memento">
+                                <div class="rounded shadow">
+                                    <img class="img-fluid rounded-top" src="/images/films/memento.jpg" alt="">
+                                    <div class="p-3">
+                                        <h3 class="fs-6 text-black text-center mb-0">06/08/2023 - 21h30</h3>
                                     </div>
                                 </div>
                             </a>
                         </div>
                     </div>
 
-                    <div class="mb-4 text-center">
-                        <button type="button" class="btn btn-outline-dark btn-lg bg-white border-0 animate-opacity text-black imperial-bold shadow">Plus de films</button>
+                    <div class="mb-5 text-center">
+                        <a href="/films">
+                            <span class="badge text-bg-dark py-2 fs-3 animate-opacity">
+                                <h3 class="imperial-bold mb-0 fw-bold">Plus de films</h3>
+                            </span>
+                        </a>
+                    </div>
+
+                    <h2 class="mb-3 imperial-bold text-black text-center text-lg-start">Plan du festival</h2>
+
+                    <div class="mb-4">
+                        <iframe class="w-100" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2623.873553136649!2d2.306766315824663!3d48.879687007160825!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66fbe98f714c3%3A0xe62425fddeddc402!2sParc%20Monceau!5e0!3m2!1sfr!2sfr!4v1680574682039!5m2!1sfr!2sfr" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
+
+                    <div class="mb-5 text-center">
+                        <a href="/reservation">
+                            <span class="badge text-bg-dark py-2 fs-3 animate-opacity">
+                                <h3 class="imperial-bold mb-0 fw-bold">Réserver</h3>
+                            </span>
+                        </a>
                     </div>
                 </section>
             </main>
