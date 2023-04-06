7ec507cf663b336c31f7846849d8f55f100c778a
arcausin <arcausin@gmail.com>
2023-04-06 17:16:27 +0200

    add unique article page

---
+++ actualites/participez-au-vote-du-public/index.php
@@ -0,0 +1,170 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Participez au vote du public - Actualités - Films en Plein Air</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="/styles.css">
+
+    <script>
+        document.addEventListener("DOMContentLoaded", function() {
+            let windowHeight = window.innerHeight;
+
+            let headerHeight = document.querySelector("#header-content").offsetHeight;
+
+            let footerHeight = document.querySelector("#footer-content").offsetHeight;
+
+            let bodyHeight = windowHeight - footerHeight;
+
+            // Ajouter la hauteur de la barre de navigation au "padding-top" du conteneur de contenu
+            document.querySelector("#main-content").style.paddingTop = headerHeight + "px";
+            document.querySelector("#main-content").style.minHeight = bodyHeight + "px";
+        });
+    </script>
+</head>
+<body>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/include/header.php'); ?>
+
+    <div class="bg-f7f4ee">
+        <div id="main-content" class="container">
+            <main class="row">
+                <section class="col-12 col-lg-8">
+                    <p class="mt-3 mb-3 text-black"><a class="text-black animate-opacity" href="/">Accueil</a> > <a class="text-black animate-opacity" href="/actualites">Actualités</a> > Participez au vote du public</p>
+
+                    <h1 class="mt-5 mb-5 imperial-bold text-black text-center text-md-start">Participez au vote du public</h1>
+
+                    <img class="shadow img-fluid rounded-4 mb-3" src="/images/actualites/participez-au-vote-du-public.jpg" alt="">
+
+                    <p class="imperial text-black fs-5 mb-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto nemo rem consequatur? Sapiente, delectus! Consequatur non architecto nobis? Voluptatem iure illum cum eligendi veritatis in maiores, quia at saepe laborum?</p>
+
+                    <hr>
+
+                    <h2 class="imperial-bold text-black mb-3">Etiam cursus vulputate dui id tincidunt</h2>
+
+                    <p class="imperial text-black fs-5 mb-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean finibus, tortor laoreet lacinia commodo, turpis lorem sagittis neque, sed accumsan urna odio sit amet tortor. Proin maximus nunc non turpis fermentum placerat. Cras sodales ante augue, in ullamcorper quam sagittis in. Mauris fringilla enim at magna ullamcorper efficitur. Nam eleifend ipsum sit amet neque luctus placerat eget nec est. Morbi pulvinar, lorem eget venenatis viverra, nulla nunc auctor nunc, vel vulputate dui urna at libero. Duis at eros tortor. Nam a malesuada nisi. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas et dapibus justo, non cursus libero. Donec non pellentesque ante, nec lacinia velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Quisque et nunc arcu. Sed feugiat facilisis lacus, eget malesuada erat tincidunt eu.</p>
+
+                    <h2 class="imperial-bold text-black mb-3">Aliquam vestibulum libero ipsum</h2>
+
+                    <p class="imperial text-black fs-5 mb-3">Nullam mollis rutrum erat a sodales. Aliquam nec aliquet lectus, ac condimentum purus. Morbi lorem dolor, rhoncus sed mauris et, feugiat auctor enim. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Vivamus blandit commodo risus quis tempus. Proin id risus porta, tempor quam feugiat, malesuada enim. Pellentesque imperdiet lectus nec elit mattis, eget bibendum massa maximus. Cras pretium, libero quis elementum iaculis, quam felis ornare neque, in scelerisque est nisi ac lorem. Vivamus sem nunc, semper tempus porttitor ac, convallis eu est. In laoreet vitae justo accumsan placerat. Aenean vehicula, velit in ornare efficitur, tortor neque vehicula massa, vel congue diam enim id turpis. Fusce vulputate et ligula non feugiat. Sed auctor pulvinar ante.</p>
+
+                    <h2 class="imperial-bold text-black mb-3">Vitae mattis mi nunc eget libero</h2>
+
+                    <p class="imperial text-black fs-5 mb-3">Aliquam erat volutpat. Praesent ultricies egestas maximus. Quisque facilisis a nisl id interdum. Aliquam lobortis mattis tellus. Vestibulum mi turpis, dictum ut lobortis at, tempus eu dolor. Nullam vehicula, tellus feugiat euismod consequat, mi orci egestas nunc. Etiam pulvinar aliquam sem, quis hendrerit urna euismod sed. Proin nec scelerisque arcu, vitae pellentesque nunc. Fusce et porta nisl. Nam lectus ipsum, molestie eget condimentum sit amet, ornare sed urna. Fusce lacinia, felis ac lobortis consequat, purus neque ultricies justo, et placerat metus justo quis enim.</p>
+
+                    <hr>
+                </section>
+                <section class="col-12 col-lg-4">
+                    <h3 class="imperial-bold mt-0 mt-lg-3 mb-3 text-black">À lire aussi</h3>
+
+                    <div class="row g-3 mb-5">
+                        <div class="col-12 col-md-6 col-lg-12">
+                            <a class="text-decoration-none text-white animate-opacity" href="/actualites/la-projection-d-emily-the-criminal-en-presence-du-realisateur">
+                                <div class="position-relative">
+                                    <img class="shadow img-fluid rounded-4" src="/images/actualites/la-projection-d-emily-the-criminal-en-presence-du-realisateur.jpg" alt="" style="filter: brightness(0.85);">
+
+                                    <div class="p-3 position-absolute bottom-0">
+                                        <h3 class="fs-4 imperial-bold mb-0">La projection d'Emily The Criminal en présence du réalisateur</h3>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+
+                        <div class="col-12 col-md-6 col-lg-12">
+                            <a class="text-decoration-none text-white animate-opacity" href="/actualites/la-selection-du-festival">
+                                <div class="position-relative">
+                                    <img class="shadow img-fluid rounded-4" src="/images/actualites/la-selection-du-festival.jpg" alt="" style="filter: brightness(0.85);">
+
+                                    <div class="p-3 position-absolute bottom-0">
+                                        <h3 class="fs-4 imperial-bold mb-0">La sélection du festival</h3>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+
+                    <h3 class="imperial-bold mt-0 mt-lg-3 mb-3 text-black">Les prochaines projections</h3>
+
+                    <div class="row g-3 mb-5">
+                        <div class="col-6 col-md-4 col-lg-6">
+                            <a class="text-decoration-none text-white animate-opacity" href="/films/paranoia">
+                                <div class="rounded shadow">
+                                    <img class="img-fluid rounded-top" src="/images/films/paranoia.jpg" alt="">
+                                    <div class="p-3">
+                                        <h3 class="fs-6 text-black text-center mb-0">05/08/2023 - 18h00</h3>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+
+                        <div class="col-6 col-md-4 col-lg-6">
+                            <a class="text-decoration-none text-white animate-opacity" href="/films/nocturnal-animals">
+                                <div class="rounded shadow">
+                                    <img class="img-fluid rounded-top" src="/images/films/nocturnal_animals.jpg" alt="">
+                                    <div class="p-3">
+                                        <h3 class="fs-6 text-black text-center mb-0">05/08/2023 - 20h00</h3>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+
+                        <div class="col-6 col-md-4 col-lg-6">
+                            <a class="text-decoration-none text-white animate-opacity" href="/films/prisoners">
+                                <div class="rounded shadow">
+                                    <img class="img-fluid rounded-top" src="/images/films/prisoners.jpg" alt="">
+                                    <div class="p-3">
+                                        <h3 class="fs-6 text-black text-center mb-0">05/08/2023 - 22h00</h3>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+
+                        <div class="col-6 col-md-4 col-lg-6">
+                            <a class="text-decoration-none text-white animate-opacity" href="/films/dual">
+                                <div class="rounded shadow">
+                                    <img class="img-fluid rounded-top" src="/images/films/dual.jpg" alt="">
+                                    <div class="p-3">
+                                        <h3 class="fs-6 text-black text-center mb-0">06/08/2023 - 18h00</h3>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+
+                        <div class="col-6 col-md-4 col-lg-6">
+                            <a class="text-decoration-none text-white animate-opacity" href="/films/a-ghost-story">
+                                <div class="rounded shadow">
+                                    <img class="img-fluid rounded-top" src="/images/films/a_ghost_story.jpg" alt="">
+                                    <div class="p-3">
+                                        <h3 class="fs-6 text-black text-center mb-0">06/08/2023 - 19h45</h3>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+
+                        <div class="col-6 col-md-4 col-lg-6">
+                            <a class="text-decoration-none text-white animate-opacity" href="/films/memento">
+                                <div class="rounded shadow">
+                                    <img class="img-fluid rounded-top" src="/images/films/memento.jpg" alt="">
+                                    <div class="p-3">
+                                        <h3 class="fs-6 text-black text-center mb-0">06/08/2023 - 21h30</h3>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </section>
+                
+                <h2 class="mb-3 imperial-bold text-black text-center text-lg-start">Plan du festival</h2>
+
+                <div class="mb-5">
+                    <iframe class="shadow rounded-4 w-100" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2623.873553136649!2d2.306766315824663!3d48.879687007160825!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66fbe98f714c3%3A0xe62425fddeddc402!2sParc%20Monceau!5e0!3m2!1sfr!2sfr!4v1680574682039!5m2!1sfr!2sfr" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+            </main>
+        </div>
+    </div>
+
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/include/footer.php'); ?>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</body>
+</html>
