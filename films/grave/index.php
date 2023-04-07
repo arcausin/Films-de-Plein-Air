c982c60797757936047a99a6d4cd7ea5199482c3
arcausin <arcausin@gmail.com>
2023-04-07 04:37:54 +0200

    maj layout films pages

--- films/grave/index.php
+++ films/grave/index.php
@@ -29,7 +29,7 @@
     <div class="bg-f7f4ee">
         <div id="main-content" class="container">
             <main class="row">
-                <section class="col-12">
+                <section class="col-12 col-lg-8">
                     <p class="mt-3 mb-3 text-black"><a class="text-black animate-opacity" href="/">Accueil</a> > <a class="text-black animate-opacity" href="/films">Films</a> > Grave</p>
 
                     <h1 class="mt-5 mb-5 imperial-bold text-black text-center text-md-start">Grave</h1>
@@ -59,12 +59,112 @@
                         </a>
                     </div>
 
-                    <h2 class="mb-3 imperial-bold text-black text-center text-lg-start">Plan du festival</h2>
+                    <h3 class="imperial-bold mb-3 text-black">Nos derniers articles</h3>
 
-                    <div class="mb-5">
-                        <iframe class="shadow rounded-4 w-100" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2623.873553136649!2d2.306766315824663!3d48.879687007160825!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66fbe98f714c3%3A0xe62425fddeddc402!2sParc%20Monceau!5e0!3m2!1sfr!2sfr!4v1680574682039!5m2!1sfr!2sfr" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    <div class="row g-4 mb-5">
+                        <div class="col-12 col-md-6 col-lg-6">
+                            <a class="text-decoration-none text-white animate-opacity" href="/actualites/la-projection-d-emily-the-criminal-en-presence-du-realisateur">
+                                <div class="position-relative">
+                                    <img class="shadow img-fluid rounded-4" src="/images/actualites/la-projection-d-emily-the-criminal-en-presence-du-realisateur.jpg" alt="" style="filter: brightness(0.85);">
+
+                                    <div class="p-3 position-absolute bottom-0">
+                                        <h4 class="imperial-bold mb-0">La projection d'Emily The Criminal en présence du réalisateur</h4>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+
+                        <div class="col-12 col-md-6 col-lg-6">
+                            <a class="text-decoration-none text-white animate-opacity" href="/actualites/participez-au-vote-du-public">
+                                <div class="position-relative">
+                                    <img class="shadow img-fluid rounded-4" src="/images/actualites/participez-au-vote-du-public.jpg" alt="" style="filter: brightness(0.85);">
+
+                                    <div class="p-3 position-absolute bottom-0">
+                                        <h4 class="imperial-bold mb-0">Participez au vote du public</h4>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
                     </div>
                 </section>
+
+                <section class="col-12 col-lg-4">
+                    <h3 class="mb-3 imperial-bold text-black mt-0 mt-lg-3">Les prochaines projections</h3>
+
+                    <div class="row g-3 mb-5">
+                        <div class="col-6 col-md-4 col-lg-6">
+                            <a class="text-decoration-none text-white animate-opacity" href="/films/paranoia">
+                                <div class="rounded shadow">
+                                    <img class="img-fluid rounded-top" src="/images/films/paranoia.jpg" alt="">
+                                    <div class="p-3">
+                                        <h3 class="fs-6 text-black text-center mb-0">05/08/2023 - 18h00</h3>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+
+                        <div class="col-6 col-md-4 col-lg-6">
+                            <a class="text-decoration-none text-white animate-opacity" href="/films/nocturnal-animals">
+                                <div class="rounded shadow">
+                                    <img class="img-fluid rounded-top" src="/images/films/nocturnal_animals.jpg" alt="">
+                                    <div class="p-3">
+                                        <h3 class="fs-6 text-black text-center mb-0">05/08/2023 - 20h00</h3>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+
+                        <div class="col-6 col-md-4 col-lg-6">
+                            <a class="text-decoration-none text-white animate-opacity" href="/films/prisoners">
+                                <div class="rounded shadow">
+                                    <img class="img-fluid rounded-top" src="/images/films/prisoners.jpg" alt="">
+                                    <div class="p-3">
+                                        <h3 class="fs-6 text-black text-center mb-0">05/08/2023 - 22h00</h3>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+
+                        <div class="col-6 col-md-4 col-lg-6">
+                            <a class="text-decoration-none text-white animate-opacity" href="/films/dual">
+                                <div class="rounded shadow">
+                                    <img class="img-fluid rounded-top" src="/images/films/dual.jpg" alt="">
+                                    <div class="p-3">
+                                        <h3 class="fs-6 text-black text-center mb-0">06/08/2023 - 18h00</h3>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+
+                        <div class="col-6 col-md-4 col-lg-6">
+                            <a class="text-decoration-none text-white animate-opacity" href="/films/a-ghost-story">
+                                <div class="rounded shadow">
+                                    <img class="img-fluid rounded-top" src="/images/films/a_ghost_story.jpg" alt="">
+                                    <div class="p-3">
+                                        <h3 class="fs-6 text-black text-center mb-0">06/08/2023 - 19h45</h3>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+
+                        <div class="col-6 col-md-4 col-lg-6">
+                            <a class="text-decoration-none text-white animate-opacity" href="/films/memento">
+                                <div class="rounded shadow">
+                                    <img class="img-fluid rounded-top" src="/images/films/memento.jpg" alt="">
+                                    <div class="p-3">
+                                        <h3 class="fs-6 text-black text-center mb-0">06/08/2023 - 21h30</h3>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </section>
+
+                <h2 class="mb-3 imperial-bold text-black text-center text-lg-start">Plan du festival</h2>
+
+                <div class="mb-5">
+                    <iframe class="shadow rounded-4 w-100" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2623.873553136649!2d2.306766315824663!3d48.879687007160825!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66fbe98f714c3%3A0xe62425fddeddc402!2sParc%20Monceau!5e0!3m2!1sfr!2sfr!4v1680574682039!5m2!1sfr!2sfr" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
             </main>
         </div>
     </div>
