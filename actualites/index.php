28d25fa52b04f8b6d02066b53e901ec2e11ec857
arcausin <arcausin@gmail.com>
2023-04-17 03:59:40 +0200

    replace include footer with html brut

--- actualites/index.php
+++ actualites/index.php
@@ -121,7 +121,32 @@
         </div>
     </div>
 
-    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/include/footer.php'); ?>
+    <footer id="footer-content" class="container-fluid bg-c22015">
+        <div class="row">
+            <div class="col-12 col-lg-4 py-2 d-flex align-items-center justify-content-center justify-content-lg-start">
+                <img class="img-fluid" src="/images/logo-alternative.png" alt="" style="height: 35px;">
+
+                <a class="navbar-brand animate-opacity ms-3" href="/"><p class="hardome text-white fs-3 mb-0">Les Films de Plein Air</p></a>
+            </div>
+
+            <div class="col-12 col-lg-4 align-self-lg-center">
+                <nav class="navbar-expand-lg mx-2 text-center mx-md-5 mx-lg-0 mt-2 mb-2">
+                    <ul class="navbar-nav d-lg-flex justify-content-lg-around">
+                        <li class="nav-item"><a class="text-decoration-none nav-link text-white animate-opacity" href="#">Mentions légales</a></li>
+                        <li class="nav-item"><a class="text-decoration-none nav-link text-white animate-opacity" href="#">Politique de confidentialités</a></li>
+                        <li class="nav-item"><a class="text-decoration-none nav-link text-white animate-opacity" href="#">Contact</a></li>
+                    </ul>
+                </nav>
+            </div>
+            
+            <div class="col-12 col-lg-4 d-lg-flex justify-content-lg-around mt-2 mb-2">
+                <p class="text-center text-white align-self-center mb-0">Du 5 au 8 août 2023</p>
+                <p class="text-center text-white align-self-center mb-0">Parc Monceau <br>
+                35 Boulevard de Courcelles <br>
+                75008 Paris</p>
+            </div>
+        </div>
+    </footer>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
